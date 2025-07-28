be3d9c890ce0d34188d62dd6781af5936b7395c8
Dev-Archana <asm43636@gmail.com>
2025-07-28 21:17:21 +0530

    redirect-file

--- frontend/src/components/TrainerForm.jsx
+++ frontend/src/components/TrainerForm.jsx
@@ -149,7 +149,7 @@ const TrainerForm = () => {
 
                     <div className="col-span-1 md:col-span-2 text-center">
                         <button
-                            type="submit"
+                            onClick={handleSubmit}
                             className="bg-[#1F3C88] hover:bg-blue-900 text-white px-6 py-3 rounded-lg font-semibold transition"
                         >
                             Add Trainer
